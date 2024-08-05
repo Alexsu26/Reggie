a8db441d6202badbd3284bdad8db596f8d0f34cc
Xu_no_ghost <xu_no_ghost@163.com>
2024-08-05 13:52:30 +0800

    配置redis环境

---
+++ src/main/java/com/liyang/reggie_takeout/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.liyang.reggie_takeout.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory conn) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(conn);
+
+        return redisTemplate;
+    }
+}
